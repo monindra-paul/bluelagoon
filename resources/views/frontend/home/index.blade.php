2368fa73ced93e4b3b51bc3de8b8552d38e206bf
monindra-paul <dipmonindra@gmail.com>
2023-09-15 21:52:41 +0530

    update-home slider

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -27,7 +27,7 @@
 								<!-- LAYER NR. 4 -->
 								<div class="tp-caption   tp-resizeme" id="slide-407-layer-2" data-x="['left','left','center','center']" data-hoffset="['0','50','0','0']" data-y="['top','top','top','top']" data-voffset="['0','430','460','290']" data-fontsize="['30','30','40','25']" data-lineheight="['40','40','50','30']" data-width="['none','100%','100%','100%']" data-height="none" data-whitespace="normal" data-type="text" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":"+890","split":"chars","splitdelay":0.03,"speed":300,"frame":"0","from":"sX:2;sY:2;opacity:0;fb:5px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},
 					{"delay":"wait","speed":1000,"frame":"999","to":"opacity:0;fb:10px;","ease":"Power4.easeOut"}]' data-margintop="[0,0,0,0]" data-marginright="[0,0,0,0]" data-marginbottom="[0,0,0,0]" data-marginleft="[0,0,0,0]" data-textAlign="['inherit','inherit','center','center']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 9; white-space: normal; font-size: 30px; line-height: 40px; font-weight: 300;
-					color: rgba(255, 255, 255, 1.00); display: block;font-family:Poppins;">Welcome to "The Blue Lagoon" - Where Beauty and Family Blend Perfectly!</div>
+					color: rgba(255, 255, 255, 1.00); display: block;font-family:Poppins;">Welcome to "The Blue Lagoon" - Family Hair & Beauty Salon</div>
 							</div>
 						</div>
 					</div>
